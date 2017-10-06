69d8abf34279d9c9fbe6293c9e351c60d18440e2
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 16:47:19 +0200

    Updated setup data.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from distutils.core import setup
 setup(
     name='mimeutil',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
     py_modules=['mimeutil'],
-    license=open('LICENSE').read(),
+    license='GPLv3',
     description='A MIME type and file extension library')

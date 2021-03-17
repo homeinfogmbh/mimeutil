7e01678c8452854fe748738a75f47f083ec088e7
Richard Neumann <r.neumann@homeinfo.de>
2021-03-17 11:42:05 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python3
+"""Setup script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='mimeutil',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='info@homeinfo.de',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='r.neumann@homeinfo.de',
+    install_requires=['python-magic'],
     py_modules=['mimeutil'],
     license='GPLv3',
-    description='A MIME type and file extension library.')
+    description='A MIME type and file extension library.'
+)

a2dde39f7e15c8a64ba1331259b19ee7c88eefae
Richard Neumann <mail@richard-neumann.de>
2021-11-29 20:55:35 +0100

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 #! /usr/bin/env python3
-"""Setup script."""
 
 from setuptools import setup
 
 setup(
     name='mimeutil',
-    use_scm_version={
-        "local_scheme": "node-and-timestamp"
-    },
+    use_scm_version=True,
     setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='info@homeinfo.de',
+    author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
-    maintainer_email='r.neumann@homeinfo.de',
-    install_requires=['python-magic'],
+    maintainer_email='<r dot neumann at homeinfo priod de>',
     py_modules=['mimeutil'],
     license='GPLv3',
     description='A MIME type and file extension library.'
